33c51f69a996ae2caa68798f12fd6c8a95579c15
Abdulraheem Habib Abiola <64592361+theAbiola@users.noreply.github.com>
2026-08-22 17:03:18 +0100

    fix console.log linting issue

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log('did we get here?');
   const output = 'Works on my machine.';
   res.send(output);
 });
