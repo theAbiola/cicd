8885820dc4e6ebaba8156acbccb5a72ffe0de0ef
Abdulraheem Habib Abiola <64592361+theAbiola@users.noreply.github.com>
2026-08-23 15:27:32 +0100

    add docker actions to ci

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 const port = 3000;
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   // eslint-disable-next-line no-console
   console.log(`Server listening on port ${port}`);
 });
